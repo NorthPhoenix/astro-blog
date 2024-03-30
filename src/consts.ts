ccfd9243a066a85c4be882a6d224b7b6fa31094e
Nikita Istomin <nikita.istomin54@gmail.com>
2024-03-30 00:11:10 -0500

    chore: Updated site metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog"
-export const SITE_DESCRIPTION = "Welcome to my website!"
+export const SITE_TITLE = "Nikita's Blog"
+export const SITE_DESCRIPTION =
+  "I write about my learning journey in Three.js, and other webdev topics."
 export const FALLBACK_BLOG_HERO_IMAGE = "/src/assets/test-blog-hero-image.png"
